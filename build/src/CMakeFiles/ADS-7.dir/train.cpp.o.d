ad8e32efa260f2d09082fcd888aaec454b91845b
andreikaar <a.karzhenkov@inbox.ru>
2026-06-06 12:33:55 +0300

    fix: remove build from repo

--- build/src/CMakeFiles/ADS-7.dir/train.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/ADS-7.dir/train.cpp.o: \
-  /Users/andreikar/Desktop/algr/ADS-7/src/train.cpp \
-  /Users/andreikar/Desktop/algr/ADS-7/include/train.h
--- build/src/CMakeFiles/ADS-7.info.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ADS-7.info.
-# This may be replaced when dependencies are built.
